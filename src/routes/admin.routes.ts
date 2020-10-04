0d7b51daf6eb26f350ead5cad17a9fd29263023b
JulioHey <juliohey.dev@gmail.com>
2020-10-04 17:07:50 -0300

    Dale

--- src/routes/admin.routes.ts
+++ src/routes/admin.routes.ts
@@ -4,14 +4,8 @@ import {teamController} from '../controllers/team.controller';
 
 const adminRoutes = express.Router();
 
-adminRoutes.post("/teams", teamController.handleAddTeam);
-
-<<<<<<< Updated upstream
-export default adminRoutes;
-=======
 adminRoutes.post("/", (request: Request, response: Response) => {
         return teamController.addTeam(request, response);
 });
 
 export {adminRoutes};
->>>>>>> Stashed changes
