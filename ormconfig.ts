ae4146d04f7675a48cc61c3f0e2dca6db465a0f8
JulioHey <juliohey.dev@gmail.com>
2020-10-26 17:16:22 -0300

    More configuration

--- ormconfig.ts
+++ ormconfig.ts
@@ -2,14 +2,15 @@ require('dotenv').config({
     path: process.env.NODE_ENV === "test" ? ".env.test" : ".env"
 });
 
+console.log(process.env.DB_LOCATION ,process.env.DB_NAME, process.env.DB_DIALECT, process.env.DB_PASS)
+
 
 module.exports = {
     type: process.env.DB_DIALECT || "postgres",
     username: process.env.DB_USER,
-    database: process.env.DB_NAME,
+    database: process.env.DB_LOCATION || "postgres",
     password: process.env.DB_PASS,
     logging: true,
-    storage: "./__tests__/database.sqlite",
     entities: [
        "src/models/**/*.model.ts"
     ],
